0a69035002d4b15a966ed79e4d8ad122da317506
Eyobed <eyobedteshome@gmail.com>
2023-07-07 14:56:29 +0300

    Update

--- hospital/app.py
+++ hospital/app.py
@@ -1,16 +1,19 @@
 import os
 import sqlite3
 
+from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
+from flask_autoreload import AutoReload
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
+
 
 from helpers import login_required
 
 # Configure application
 app = Flask(__name__)
+app.run(debug=True)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
