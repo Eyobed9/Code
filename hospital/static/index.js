fda6b7149129e2a34904f69a5cac981c348f9749
Eyobed <eyobedteshome@gmail.com>
2023-07-12 12:19:45 +0300

    Update

--- hospital/static/index.js
+++ hospital/static/index.js
@@ -1,20 +1,20 @@
 function physicians(name)
 {
     // if nothing is chosen
-    if (name == " ") {
+    if (name == "") {
         return;
-    }
-
+    } 
+        
     // create a new ajax object
     var ajax = new XMLHttpRequest();
 
     ajax.onreadystatechange = function() {
         if (ajax.readyState == 4 && ajax.status == 200) {
-           $('#info').html(ajax.responseText);
+           document.getElementById('info').innerHTML = ajax.responseText;
         }
     };
 
     // open the requested file and transmit the date
-    ajax.open('GET', name + '.html', true)
+    ajax.open('GET', '/templates/' + name + '.html', true);
     ajax.send();   
 }
