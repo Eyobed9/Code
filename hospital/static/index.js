27b5da8c99f6f4290f92ad21bc23aaa47e7522db
Eyobed <eyobedteshome@gmail.com>
2023-07-22 12:03:27 +0300

    Update on 22/7/2023

--- hospital/static/index.js
+++ hospital/static/index.js
@@ -21,3 +21,23 @@ function physicians(name)
     ajax.open('GET', '/physicians?name=' + name, true);
     ajax.send();   
 }
+
+const imageContainer = document.getElementById("imageContainer");
+const images = imageContainer.getElementsByTagName("img");
+let index = 0;
+
+function changeImage() {
+   // Hide all images
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+    // increment the index
+    index = (index + 1) % images.length;
+
+    // Show the next image
+    images[index].style.display = "block";
+}
+// Call the changeImage function initially to show the first image
+changeImage();
+// Call the changeImage function every 2.5 seconds
+setInterval(changeImage, 2500);
