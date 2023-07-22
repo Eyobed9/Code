274c86c1b8ee46c7032b3126763f05eb3b5457fc
Eyobed <eyobedteshome@gmail.com>
2023-07-22 12:37:09 +0300

    done slide-show images

--- hospital/static/index.js
+++ hospital/static/index.js
@@ -22,22 +22,25 @@ function physicians(name)
     ajax.send();   
 }
 
-const imageContainer = document.getElementById("imageContainer");
-const images = imageContainer.getElementsByTagName("img");
-let index = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const imageContainer = document.getElementById("imageContainer");
+    const images = imageContainer.getElementsByTagName("img");
+    let index = 0;
+    changeImage();
 
-function changeImage() {
-   // Hide all images
-  for (let i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
-  }
-    // increment the index
-    index = (index + 1) % images.length;
 
-    // Show the next image
-    images[index].style.display = "block";
-}
-// Call the changeImage function initially to show the first image
-changeImage();
-// Call the changeImage function every 2.5 seconds
-setInterval(changeImage, 2500);
+    function changeImage() {
+    // Hide all images
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+        // increment the index
+        index = (index + 1) % images.length;
+
+        // Show the next image
+        images[index].style.display = "block";
+    }
+
+    // Call the changeImage function every 2.5 seconds
+    setInterval(changeImage, 4500);
+});
